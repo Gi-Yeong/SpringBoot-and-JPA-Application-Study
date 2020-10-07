af3efe26e6b434b39b37eab64f9c27bca814d5f9
kiyoung85 <kiyoung85@gmail.com>
2020-10-08 02:36:25 +0900

    회원 리포지토리 개발

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public void save(Member member) {
+        entityManager.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return entityManager.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return entityManager.createQuery("select m From Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return entityManager.createQuery("select m From Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+
+    }
+}
