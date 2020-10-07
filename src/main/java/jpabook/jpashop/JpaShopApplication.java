dbe383a398a1cd45be61ed0faea1568cbf4ad29c
kiyoung85 <kiyoung85@gmail.com>
2020-10-07 17:48:46 +0900

    JPA 와 DB 설정 및 동작 확인

--- src/main/java/jpabook/jpashop/JpaShopApplication.java
+++ src/main/java/jpabook/jpashop/JpaShopApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JpashopApplication {
+public class JpaShopApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JpashopApplication.class, args);
+        SpringApplication.run(JpaShopApplication.class, args);
     }
 
 }
