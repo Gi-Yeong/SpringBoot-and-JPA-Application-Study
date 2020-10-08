524255b23d26dcfde0b6b8d9a91670e01aed7e23
kiyoung85 <kiyoung85@gmail.com>
2020-10-09 00:16:14 +0900

    주문, 주문상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -20,5 +20,5 @@ public class Delivery {
     private Address address;
 
     @Enumerated(EnumType.STRING)
-    private DeliveryStatus status; // READY, CAMP
+    private DeliveryStatus status; // READY, COMP
 }
