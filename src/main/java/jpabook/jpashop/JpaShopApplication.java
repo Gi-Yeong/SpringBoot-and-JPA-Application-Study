e5fefc0c0ad7891bcf622f3ae9484dec28077465
kiyoung85 <kiyoung85@gmail.com>
2020-10-11 00:22:48 +0900

    간단한 주문 조회V1 : 엔티티 직접 노출

--- src/main/java/jpabook/jpashop/JpaShopApplication.java
+++ src/main/java/jpabook/jpashop/JpaShopApplication.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpaShopApplication {
@@ -10,4 +12,10 @@ public class JpaShopApplication {
         SpringApplication.run(JpaShopApplication.class, args);
     }
 
+    @Bean
+    Hibernate5Module hibernate5Module() {
+        Hibernate5Module hibernate5Module = new Hibernate5Module();
+//        hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+        return hibernate5Module;
+    }
 }
---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * X to One(ManyToOne, OneToOne
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> allByString = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : allByString) {
+            order.getMember().getName(); // Lazy 강제 초기화
+            order.getDelivery().getAddress();
+        }
+        return allByString;
+    }
+}
